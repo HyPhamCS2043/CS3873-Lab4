ac96d0f1f53c05b7ab2c85798f4797d146e7e912
HyPhamCS2043 <giahy.baohan@gmail.com>
2025-02-28 15:52:14 -0400

    Create the main while loop. Initialize connectionSocket. Add display messages to indicate successful connection AND to indicate server waiting for input.

--- CalcTCPServer.java
+++ CalcTCPServer.java
@@ -2,8 +2,9 @@
  * A TCP Server class that computes simple arithmetic equations
  *  Input:  lines of arithematic questions (as String), obtained from 
  *          CalcTCPClient application.
- *  Output: the answer to each arithmetic question (converted toString), send to 
+ *  Output: the answer to each arithmetic question (converted to String), send to 
  *          CalcTCPClient line-by-line.
+ * 
  * Author: Hy Pham
  * Date Created: Feb 28, 2025
  */
@@ -13,9 +14,26 @@ import java.net.*;
 
 public class CalcTCPServer {
     public static void main (String args[]) throws Exception {
+        // String containing the aritmethic problem to be calculated
         String arithQuestion;
+
+        // String containing the computed value of the arithmetic question in
+        // arithQuestion
         String arithAnswer;
+
+        // ServerSocket to accept connection from CalcTCPClient through port 2424
         ServerSocket welcomeSocket = new ServerSocket(2424);
+
         System.out.println("Waiting for Connection From CalcTCPClient ...");
+
+        //While loop is always true to enable server to run until connection is closed
+        while (true) {
+            //
+            Socket connectionSocket = welcomeSocket.accept();
+
+            System.out.println ("Connection to CalcTCPClient successful!");
+		    System.out.println ("Waiting for input from CalcTCPClient.....");
+
+        }
     }
 }
