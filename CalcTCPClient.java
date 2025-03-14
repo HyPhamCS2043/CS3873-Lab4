9199b35b7949ee59f9e20af2b71e08871e33fb15
Hy Pham <giahy.baohan@gmail.com>
2025-03-13 22:24:02 -0300

    Add variable to store total char sent by client and received from server to CaclTCPClient. In the while loop, add the length of the input string to server and output string from server to these variables. Update last print statement to include these

--- CalcTCPClient.java
+++ CalcTCPClient.java
@@ -14,12 +14,6 @@
 public class CalcTCPClient {
     public static void main(String args[]) throws Exception {
 
-        //String variable to hold the equation from textfile
-		String equation;
-
-        //String variable to hold the answer to an equation
-		String arithmeticOutput;
-
         //Input stream to accept String input from text file
 		BufferedReader inFromUser = new BufferedReader(new FileReader(args[0]));
 
@@ -31,6 +25,17 @@ public class CalcTCPClient {
 		BufferedReader inFromCalcServer = new BufferedReader(new InputStreamReader(
 				clientSocket.getInputStream()));
 
+        //String variable to hold the equation from textfile
+		String equation;
+
+        //String variable to hold the answer to an equation
+		String arithmeticOutput;
+
+        //Integer variable to hold the number of char sent by client
+        int totalCharSent = 0;
+
+        //Integer variable to hold the number of char received from server
+        int totalCharReceived = 0;
        
         //integer i to denote question number        
         int i = 0;
@@ -49,27 +54,50 @@ public class CalcTCPClient {
             //Send the line of String to server for computation
             outToCalcServer.writeBytes(equation + '\n');
 
+            totalCharSent += equation.length();
+
             //Store the output from the server into arithmeticOutput
             arithmeticOutput = inFromCalcServer.readLine();
 
+            totalCharReceived += arithmeticOutput.length();
+
             //Print out the question and the answer
             System.out.println("#" + i + "--------------------------------\n" 
-                            + "Question from client:" + equation + "\n"
-                            + "Answer from server:" + arithmeticOutput + "\n"
+                            + "Question from client: " + equation + "\n"
+                            + "Answer from server: " + arithmeticOutput + "\n"
             );
 
             equation = inFromUser.readLine();
 
         }
 
-        equation = "DONE/n";
+        //Sending the server "DONE" to verify that there is no question left
+        equation = "DONE\n";
         outToCalcServer.writeBytes(equation + '\n');
-    
+        totalCharSent += equation.length();
+
+        arithmeticOutput = inFromCalcServer.readLine();
+        totalCharReceived += arithmeticOutput.length();
+
         //Stop measuring execution time
         long endTime = System.nanoTime(); 
-		
+                
         //Calculating the average time 
         long averageTime = ((endTime - startTime) / i) / 1000000;
+
+        //Check if the server has received the last message: "DONE\n"
+        if (arithmeticOutput == "CLOSE\n") {
+            //Print out the summary of the session
+            System.out.println("Q&A END**********************\n" 
+                            + "Total characters sent: " + totalCharSent + "\n"
+                            + "Total characters received: " + totalCharReceived + "\n"
+                            + "Average turn-around delay per question-answer: " + averageTime + "ms\n"
+            );
+        } else {
+            System.out.println("Did not receive confirmation from Server\n");
+        }
+    
+      
         
         inFromUser.close();
 		clientSocket.close();
